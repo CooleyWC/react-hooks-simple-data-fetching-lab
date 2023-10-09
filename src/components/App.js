db732d0418cb3684773d0cbce48cf325a27ae04b
Will Cooley <william.cooley@gmail.coom>
2023-10-09 10:25:32 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+
+import React, { useState, useEffect } from "react";
+
+function App(){
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [images, setImages] = useState(null)
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then((data)=>{
+            setImages(data)
+            setIsLoaded(true);
+        })
+ 
+}, [])
+
+console.log(images)
+
+if(!isLoaded) return <p>"Loading...""
+</p>
+
+    return(
+        <div>
+            <img src={images.message} alt="A Random Dog"></img>
+        </div>
+    )
+}
+
+export default App
